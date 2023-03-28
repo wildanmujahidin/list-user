ab13b6797c9c5ff12f8f3022251b3d702336b858
Wildan Mujahidin <wildan.mujahidin1@gmail.com>
2023-03-29 01:23:30 +0700

    build: remove redux and @redux-devtools/core depedency

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./routes/App";
 import "./styles/index.css";
+import store from "./utils/store/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
